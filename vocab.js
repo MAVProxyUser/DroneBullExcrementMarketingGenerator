1290293c27ee16909266edff7c36a370146ca065
Lead <marc@datamafia.com>
2015-05-03 11:52:05 -0400

    Throughout history, humans have been interacting with the atmosphere via ultrasonic video.

--- vocab.js
+++ vocab.js
@@ -9,131 +9,444 @@
  */
 
 var nCosmos = [
-  'cosmos',
-  'biosphere'
+  'air traffic tower',
+  'airspace',
+  'inner atmosphere',
+  'sky',
+  'foggy over region',
+  'not inside area',
+  'away from airport',
+  'local park',
+  'above ground',
+  'netherworld',
+  'atmosphere',
+  'planet',
+  'galaxy',
+  'national airspace',
+  'under 400 feet',
+  'above the grass',
+  'treeline'
 ];
 
 var nPerson = [
-  'being',
-  'seeker'
+  'pilot',
+  'drone flyer',
+  'aviator',
+  'operator',
+  'controller',
+  'human',
+  'person',
+  'minion',
+  'joyseeker'
 ];
 
 var nPersonPlural = [
-  'beings',
-  'seekers'
+  'pilots',
+  // 'children',
+  'drone flyers',
+  'aviators',
+  'operators',
+  'controllers',
+  'humans',
+  'persons',
+  'minions',
+  'flyers',
+  'humans',
+  'joyseekers',
+  'hobbyiests'
 ];
 
 var nMass = [
-  'consciousness',
-  'serenity'
+  'borg',
+  'online forum',
+  'FAQ page',
+  'manual',
+  'truth',
+  'existence',
+  'repair',
+  'potential',
+  'ability to fly',
+  'computational good',
+  'make sense',
+  'choice',
+  'thrust',
+  'rate flight',
+  'stabilized',
+  'risen device',
+  'amazing flight',
+  'free fall',
+  'li-po',
+  'battery',
+  'a 4s pack',
+  'a 3s pack',
+  'a high KV motor',
+  'a low KV motor',
+  'magic sauce',
+  'video recording',
+  'instruction manual'
 ];
 
 var nMassBad = [
-  'pain',
-  'materialism'
+  'turbulence',
+  'prop wash',
+  'flock of birds',
+  'prop cuts',
+  'Phantom pilots',
+  '3DR users',
+  'Parrot users',
+  'Hobby King corporate',
+  'fans of Chris Anderson',
+  'fans of Eric Cheng',
+  'DJI user base',
+  'flyaway pilots',
+  'grumpy legacy RC pilots',
+  'AMA',
+  'fans of Colin Guinn',
+  'drunk pilots',
+  'glue sniffers',
+  'asshats',
+  'loose screws',
+  'lacking threadlock',
+  'VTX ground short'
 ];
 
 var nOurPlural = [
-  'lives',
-  'essences'
+  'quads',
+  'multi-coptors',
+  'multi-rotors',
+  'flying devices',
+  'frames',
+  'prop spinners',
+  'flight computers',
+  'transmitters',
+  'rate flyers',
+  'racers',
+  'multirotor pilots'
 ];
 
 var nPath = [
   'circuit',
-  'mission'
+  'power dostribution board',
+  'GPS path',
+  'programmed route',
+  'RTH location',
+  'flyaway',
+  'flight plan',
+  'flight data'
 ];
 
 var nOf = [
-  'uprising',
-  'explosion'
+  'unspeakable awesome',
+  'a better build',
+  'greater discharge rate',
+  'infinite flight time',
+  'a perfect lap',
+  'forward flip',
+  'fervent speed',
+  'revolution',
+  'unbridaled dexterity',
+  'faint',
+  'source code',
+  'flashed ESC',
+  'forward tilt',
+  'freefall',
+  'rocket powered',
+  'city mission',
+  'poaching the sky',
+  'zero wind',
+  'clear skies',
+  'inspiring flight',
+  'perfect flight',
+  'harmonizing',
+  'reimagining',
+  'rekindling',
+  'unifying',
+  'CCD captured',
+  'downlink transmitted',
+  'rapid ascent',
+  'great power'
 ];
 
 var ing = [
-  'condensing',
-  'ennobling'
+  'hovering',
+  'flying',
+  'diving',
+  'filming',
+  'recovering',
+  'launching',
+  'hacking',
+  'modding',
+  'chasing',
+  'disrupting',
+  'series A funding',
+  'meditating',
+  'enabling'
 ];
 
 var adj = [
-  'joyous',
-  'sublime'
+  'stabilized',
+  'GPS enabled',
+  'acro flown',
+  'rate driven',
+  'racing PID having',
+  'high velocity',
+  'rapid ascension',
+  'rapid',
+  'good piloting',
+  'great flying',
+  'mental',
+  'coffee addicted',
+  'not drunk',
+  'not currently high',
+  'sober yet incompotent',
+  'stick tweaking',
+  'configuration obsessed',
+  'PID wrangling',
+  'forum stalking',
+  'high KV',
+  'stunt flying',
+  'proximity ninja',
+  'dive bombing',
+  'rapid discharging',
+  'high C rated',
+  'large mah having',
+  'big prop flying',
+  'mini quad user',
+  'infinite roll',
+  'highest expo',
+  'dual rate using',
+  'ancient flight',
+  'hovering',
+  'data logging',
+  'endless battery',
+  'flight-affirming',
+  'always HD recorded',
+  'stunt piloting',
+  'magic control',
+  'black-box using',
+  'safety threatening',
+  'low KV',
+  'OSD using',
+  'wind fighting',
+  'beach flying',
+  'building crashing',
 ];
 
 var adjBig = [
+  'epic',
+  'unimaginable',
+  '700mm size',
+  'greater than 450',
+  'high payload having',
+  'enormous',
+  '15 inch prop using',
+  'illegally high',
+  'steep propeller pitch',
+  'large battery moving',
+  'incredible',
   'breathtaking',
   'staggering'
 ];
 
 var adjWith = [
+  'in flight with',
+  'hovering with',
+  'flying with',
+  'battery of',
+  'charged with',
+  'discharging',
   'bursting with',
-  'electrified with'
+  'PDB connected with'
 ];
 
 var adjPrefix = [
+  'ultra-',
+  'supra-',
   'hyper-',
-  'pseudo-'
+  'mega-',
+  'extra-'
 ];
 
-var vtMass = [
+var vtMass = [ // these all seem good
+  'inspire',
+  'integrate',
+  'ignite',
+  'discover',
+  'rediscover',
+  'foster',
+  'release',
+  'manifest',
+  'harmonize',
+  'engender',
+  'bring forth',
+  'bring about',
+  'create',
+  'spark',
+  'reveal',
   'generate',
   'leverage'
 ];
 
-var vtPerson = [
+var vtPerson = [ // also smells like good bullsht
+  'enlighten',
   'inspire',
+  'empower',
+  'unify',
+  'strengthen',
+  'recreate',
+  'fulfill',
+  'change',
+  'develop',
+  'heal',
+  'awaken',
+  'synergize',
+  'ground',
+  'bless',
   'beckon'
 ];
 
 var viPerson = [
   'exist',
-  'self-actualize'
+  'believe',
+  'hover',
+  'fly',
+  'enable GPS',
+  'fly beyond 400agl',
+  'hover over people',
+  'deliver packages',
+  'operate commercially',
+  'have FAA approval'
 ];
 
 var vtDestroy = [
   'destroy',
-  'confront'
+  'eliminate',
+  'crash',
+  'meet the ground hard',
+  'fail to fly',
+  'kiss a building',
+  'self-destruct',
+  'rid the sky of DJI',
+  'forever ground all 3DR',
+  'hit the gate',
+  'meet the road'
 ];
 
 var nTheXOf = [
-  'richness',
-  'knowledge'
+  'awesomeness',
+  'VTX version',
+  'goggle view',
+  'HD version',
+  'video',
+  'data logging',
 ];
 
 var ppPerson = [
-  're-energized',
-  'aligned'
+  'rate enabled',
+  'acro flying',
+  'GPS following',
+  'reborn',
+  'guided',
+  'aligned',
+  'gyro-stabilized'
 ];
 
-var ppThingPrep = [
+var ppThingPrep = [ // sure looks good
   'enveloped in',
+  'transformed into',
+  'nurtured by',
+  'opened by',
+  'immersed in',
+  'engulfed in',
   'baptized in'
 ];
 
 var fixedAdvP = [
   'through non-local interactions',
-  'of the creative act',
+  'inherent in flight computer',
+  'at the stabilized level',
+  'at the speed of FAA regulation',
+  'of unfathomable discharge',
+  'on a corporate scale',
+  'devoid of accelerometer',
+  'of the flight plan',
 ];
 
 var fixedAdvPPlace = [
-  'in this dimension',
-  'at home in the cosmos',
+  'in this airspace',
+  'outside VTX range',
+  'within the Godhead',
+  'at home in the AMA field',
 ];
 
 var fixedNP = [
-  'meridians',
-  'morphic resonance'
+  'expanding PPM functions',
+  'superpositions of pitch',
+  'roll enducing forces',
+  'transmitter resonance',
+  'present field structures',
+  'battery ionization',
+  'ESC impulses',
+  'a resonance discharge',
+  'lipo-electricity',
+  'ultrasonic video',
+  'sonar module data',
+  'jello creating',
+  'frequencies',
+  'four-dimensional EM entities',
+  'ultra-sentient telemetry',
+  'sub-atomic telemetry',
+  'chaos-driven transmitter',
+  'supercharged high KV',
+  'supercharged PPM',
+  'sawtooth filter',
+  'transmissions',
+  'morphogenetic right hand polarizations',
+  'VTX resonance',
+  'morphic data and telemetry'
 ];
 
 var nSubject = [
-  'affirmations',
-  'the Law of Attraction'
+  'alternative flight path',
+  'GPS',
+  'RNG',
+  'bios reboot',
+  'ESC chakra',
+  'battery soul',
+  'data healing',
+  'forked build',
+  'non-GPL',
+  'closed source',
+  'magic switch',
+  'sould of the multirotor',
+  'flight path projection',
+  'secret flight plan',
+  'USB healing',
+  'wirelessly configuring',
+  'oeration manual reading',
+  'data recording',
+  'the Law of Gavity'
 ];
 
 var vOpenUp = [
+  'open up',
+  'give us access to',
+  'enable us to access',
+  'remove the top plate to',
+  'envision a flight path toward',
+  'let us configure',
   'tap into',
-  'align us with'
+  'configure us with'
 ];
 
 var vTraverse = [
-  'wander',
+  'lap',
+  'hover',
+  'fly',
+  'engage with',
+  'go along',
+  'roam',
+  'navigate',
+  'dive bomb',
   'embark on'
 ];
